4597af8d826a49d4319ea6369cb44f61a4c756e7
Stanoid <mujahidwadmohammad@gmail.com>
2024-01-02 09:30:48 +0300

    user admin permissions

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -138,7 +138,7 @@ export default function RootLayout({ children }) {
               </a>
 
               <a
-               style={{display: cookies.get("login").user.type==3?"block":"none"}}
+               style={{display: cookies.get("login").user.type&&cookies.get("login").user.type==3?"block":"none"}}  
                 href="/tests"
                 class="mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
               >
